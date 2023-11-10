c9c2aafcb41a62f025c53e8d8731ee0320f994f2
Dean Sanders <deasande@cisco.com>
2023-11-10 11:22:00 +0000

    initial commit

---
+++ Exercise-01.py
@@ -0,0 +1,20 @@
+#Exercise 1A - Change this script so the outcome becomes 'TRUE"
+number1 = 45
+
+if number1 <= 52:
+    print('TRUE')
+else:
+    print('FALSE')
+
+#Exercise 1B - Write code to calculate and print the sum of all of the numbers in the list 'number2'
+number2 = [3, 6, 4, 3]
+
+
+
+#Exercise  1C - The following disctionary contains a number of key value pairs which are names and ages. Please print the age of Damien.
+name_dict = {
+    "David": 45,
+    "Alex": 56,
+    "Amy": 34,
+    "Damien": 37
+}
